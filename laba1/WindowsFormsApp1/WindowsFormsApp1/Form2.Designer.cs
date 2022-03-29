52e877f07086a6146e878c5159adfa0ef524a466
veroniikaao <veronikapo79@gmail.com>
2022-03-29 15:39:10 +0200

    update 2

--- laba1/WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
+++ laba1/WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -31,12 +31,18 @@ namespace WindowsFormsApp1
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(12, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(363, 17);
             this.label1.TabIndex = 0;
@@ -50,13 +56,57 @@ namespace WindowsFormsApp1
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 17);
             this.label2.TabIndex = 1;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SaveFileDialog";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OpenFileDialog";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(278, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(232, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "инструкции компонентов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-4, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(10, 22);
+            this.textBox1.TabIndex = 5;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
@@ -70,5 +120,11 @@ namespace WindowsFormsApp1
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- laba1/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
+++ laba1/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -15,13 +15,17 @@ namespace WindowsFormsApp1
         public Form2()
         {
             InitializeComponent();
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
         }
 
         
 
         private void label1_Click(object sender, EventArgs e)
         {
-            label2.Text = "Свойства: \n \n Text: определяет заголовок формы \n" +
+            label1.Text = "Свойства: \n \n Text: определяет заголовок формы \n" +
                 "Name: устанавливает имя формы - точнее имя класса, который наследуется от класса Form \n" +
                 "BackColor: указывает на фоновый цвет формы. Щелкнув на это свойство, мы сможем выбрать тот цвет, который нам подходит из списка предложенных цветов или цветовой палитры \n" +
                 "Font: задает шрифт для всей формы и всех помещенных на нее элементов управления. Однако, задав у элементов формы свой шрифт, мы можем тем самым переопределить его \n" +
@@ -29,12 +33,38 @@ namespace WindowsFormsApp1
                 "Методы: \n \n" +
                 "Activate() Активирует форму и перемещает на нее фокус. \n" +
                 "CenterToScreen() Выравнивает форму по центру текущего экрана. \n" +
-                "Close() Закрывает форму. \n"; 
+                "Close() Закрывает форму.\n \n Далее нажмите на 'инструкции компонентов' "; 
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text= "Компонент SaveFileDialog - Служит для выбора сохраняемых файлов и места их сохранения. \n \n Основные свойства: \n \n 1) CreatePrompt: при значении true в случае, если указан не существующий файл, то будет отображаться сообщение о его создании \n 2) OverwritePrompt: при значении true в случае, если указан существующий файл, то будет отображаться сообщение о том, что файл будет перезаписан \n 3) Filter: задает фильтр файлов, благодаря чему в диалоговом окне можно отфильтровать файлы по расширению.";
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Компонент OpenFileDialog - Позволяет пользователям открывать файлы в стандартном диалоговом окне. \n \n Основные свойства: \n \n 1) DefaultExt: устанавливает расширение файла, которое добавляется по умолчанию, если пользователь ввел имя файла без расширения \n 2) OverwritePrompt: при значении true в случае, если указан существующий файл, то будет отображаться сообщение о том, что файл будет перезаписан \n 3) nitialDirectory: устанавливает каталог, который отображается при первом вызове окна \n \n";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+            MessageBox.Show("Файл открыт");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Компонент SaveFileDialog - Служит для выбора сохраняемых файлов и места их сохранения.\n \n Основные свойства: \n \n 1) CreatePrompt: при значении true в случае, если указан не существующий файл, то будет отображаться сообщение о его создании \n 2) OverwritePrompt: при значении true в случае, если указан существующий файл, то будет отображаться сообщение о том, что файл будет перезаписан \n 3) Filter: задает фильтр файлов, благодаря чему в диалоговом окне можно отфильтровать файлы по расширению.\n \n Компонент OpenFileDialog - Позволяет пользователям открывать файлы в стандартном диалоговом окне. \n \n Основные свойства: \n \n 1) DefaultExt: устанавливает расширение файла, которое добавляется по умолчанию, если пользователь ввел имя файла без расширения \n 2) OverwritePrompt: при значении true в случае, если указан существующий файл, то будет отображаться сообщение о том, что файл будет перезаписан \n 3) nitialDirectory: устанавливает каталог, который отображается при первом вызове окна \n \n Нажмите на кнопки с названиями компонентов, чтобы увидеть их в работе";
         }
     }
 }
